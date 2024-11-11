d5c606a26704a781463552e77f75e76ac0f3d6ce
jakKKiki <161484106+jakKKiki@users.noreply.github.com>
2024-11-11 11:55:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from pyngrok import ngrok
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
+# 建立 Flask 應用程式
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+# 設置你的 Channel Access Token 和 Channel Secret
+CHANNEL_ACCESS_TOKEN = '你的 Channel Access Token'
+CHANNEL_SECRET = '你的 Channel Secret'
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
-# 監聽所有來自 /callback 的 Post Request
+# 設置 Webhook 處理函數
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 獲取 HTTP 請求中的 X-Line-Signature 標頭
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+    # 將請求內容轉為文本
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
+    # 驗證請求的合法性
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
-# 處理訊息
+# 設置接收訊息的事件
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # 回覆相同的訊息
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=5000)
